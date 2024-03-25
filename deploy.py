78fa3ce5e0d234f94cde550ea6206bc4e7797120
Mart Broekkamp <martbr90@gmail.com>
2024-03-25 22:34:45 +0100

    deployment script

--- deploy.py
+++ deploy.py
@@ -16,7 +16,7 @@ print("Pulling latest version")
 subprocess.run("git clean -f; git reset --hard; git pull", shell=True)
 
 # serverAddress.js is untracked by git, rename it back to the original manually
-if (os.isfile("frontend/wgo/" + str(config["cache_version"] - 1) + "_serverAddress.js")):
+if (os.path.isfile("frontend/wgo/" + str(config["cache_version"] - 1) + "_serverAddress.js")):
     os.rename("frontend/wgo/" + str(config["cache_version"] - 1) + "_serverAddress.js", "frontend/wgo/serverAddress.js")
 
 print("Updating filenames")
