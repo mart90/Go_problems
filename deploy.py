418975eb7b9d0e76f3e2175984e5fb544040750a
Mart Broekkamp <martbr90@gmail.com>
2024-03-25 22:30:50 +0100

    deployment script

--- deploy.py
+++ deploy.py
@@ -15,16 +15,18 @@ from config import config
 print("Pulling latest version")
 subprocess.run("git clean -f; git reset --hard; git pull", shell=True)
 
+os.rename("frontend/wgo/" + str(config["cache_version"] - 1) + "_serverAddress.js", "frontend/wgo/serverAddress.js")
+
 print("Updating filenames")
 filenames = []
 for filename in [f for f in os.listdir("frontend/wgo") if os.path.isfile("frontend/wgo/" + f)]:
-    filenames.append(filename)
+    filenames.append("/" + filename)
     os.rename("frontend/wgo/" + filename, "frontend/wgo/" + str(config["cache_version"]) + "_" + filename)
 for filename in [f for f in os.listdir("frontend/wgo/themes") if os.path.isfile("frontend/wgo/themes/" + f)]:
-    filenames.append(filename)
+    filenames.append("/" + filename)
     os.rename("frontend/wgo/themes/" + filename, "frontend/wgo/themes/" + str(config["cache_version"]) + "_" + filename)
 for filename in [f for f in os.listdir("frontend/css") if os.path.isfile("frontend/css/" + f)]:
-    filenames.append(filename)
+    filenames.append("/" + filename)
     os.rename("frontend/css/" + filename, "frontend/css/" + str(config["cache_version"]) + "_" + filename)
 
 print("Updating references")
@@ -33,7 +35,7 @@ for filename in [f for f in os.listdir("frontend") if os.path.isfile("frontend/"
         filedata = file.read()
 
     for file_ref in filenames:
-        filedata = filedata.replace(file_ref, str(config["cache_version"]) + "_" + file_ref)
+        filedata = filedata.replace(file_ref, "/" + str(config["cache_version"]) + "_" + file_ref)
 
     with open("frontend/" + filename, 'w') as file:
         file.write(filedata)
@@ -43,12 +45,21 @@ for filename in [f for f in os.listdir("frontend/templates") if os.path.isfile("
         filedata = file.read()
 
     for file_ref in filenames:
-        filedata = filedata.replace(file_ref, str(config["cache_version"]) + "_" + file_ref)
+        filedata = filedata.replace(file_ref, "/" + str(config["cache_version"]) + "_" + file_ref)
 
     with open("frontend/templates/" + filename, 'w') as file:
         file.write(filedata)
 
 print("Restarting gunicorn")
 subprocess.run("pkill gunicorn; gunicorn -b 127.0.0.1:5000 app:app --daemon --log-file /var/log/gunicorn_error.log", shell=True)
+
+print("Updating version number in config")
+with open("config.py", "r") as file:
+    configdata = file.read()
+
+configdata = configdata.replace('"cache_version": ' + str(config["cache_version"]), '"cache_version": ' + str(config["cache_version"] + 1))
+
+with open("config.py", "w") as file:
+    file.write(configdata)
 
 print("Done")
