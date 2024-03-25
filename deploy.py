60c6e4e532c4aea123c6cc0b5866ef27fb8f1943
Mart Broekkamp <martbr90@gmail.com>
2024-03-25 21:49:52 +0100

    deployment script

--- deploy.py
+++ deploy.py
@@ -13,39 +13,38 @@ from config import config
 
 print("Pulling latest version")
 subprocess.run("git reset --hard; git pull", shell=True)
-time.sleep(5)
 
 print("Updating filenames")
 filenames = []
 for filename in os.listdir("frontend/wgo"):
     filenames.append(filename)
-    os.rename(filename, str(config["cache_version"]) + "_" + filename)
+    os.rename("frontend/wgo/" + filename, "frontend/wgo/" + str(config["cache_version"]) + "_" + filename)
 for filename in os.listdir("frontend/wgo/themes"):
     filenames.append(filename)
-    os.rename(filename, str(config["cache_version"]) + "_" + filename)
+    os.rename("frontend/wgo/themes/" + filename, "frontend/wgo/themes/" + str(config["cache_version"]) + "_" + filename)
 for filename in os.listdir("frontend/css"):
     filenames.append(filename)
-    os.rename(filename, str(config["cache_version"]) + "_" + filename)
+    os.rename("frontend/css/" + filename, "frontend/css/" + str(config["cache_version"]) + "_" + filename)
 
 print("Updating references")
 for filename in os.listdir("frontend"):
-    with open(filename, 'r') as file:
+    with open("frontend/" + filename, 'r') as file:
         filedata = file.read()
 
     for file_ref in filenames:
         filedata = filedata.replace(file_ref, str(config["cache_version"]) + "_" + file_ref)
 
-    with open(filename, 'w') as file:
+    with open("frontend/" + filename, 'w') as file:
         file.write(filedata)
 
 for filename in os.listdir("frontend/templates"):
-    with open(filename, 'r') as file:
+    with open("frontend/templates/" + filename, 'r') as file:
         filedata = file.read()
 
     for file_ref in filenames:
         filedata = filedata.replace(file_ref, str(config["cache_version"]) + "_" + file_ref)
 
-    with open(filename, 'w') as file:
+    with open("frontend/templates/" + filename, 'w') as file:
         file.write(filedata)
 
 print("Restarting gunicorn")
